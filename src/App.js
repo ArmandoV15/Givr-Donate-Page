0319db60e2c749df30b18b886e350c9182b06cd3
Armando Valdez <armandov1015@gmail.com>
2022-01-23 22:22:48 -0800

    pk key update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import { loadStripe } from '@stripe/stripe-js'
-import { Elements } from '@stripe/react-stripe-js'
-import PaymentForm from './components/PaymentForm';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import PaymentForm from "./components/PaymentForm";
 
+const PUBLIC_KEY =
+  "pk_test_51jgfV5KEZyhei0VRXMcr9207d9fxYtKD2hk20EfYByHdtLV15LOSjoMPIWAI@AUT55uZuSP9E3Xdcf17fbOwxbEm004d0gAc8g";
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
-
-
-
-const PUBLIC_KEY = 'pk_test_51JgF9BCDmCaQpeqp1eV2eopbWwlqZ6OnPWJQNoCV2DeZDyYp0Nu1kFQn9fz0ncMuJgNaluBozb3sxxb9C787sTZd00QzOUMzWn'
-const stripeTestPromise = loadStripe(PUBLIC_KEY)
-
-  function App(){
-    return (
-        <Elements stripe={stripeTestPromise}>
-            <PaymentForm />
-        </Elements>
-    );
-  }
-
+function App() {
+  return (
+    <Elements stripe={stripeTestPromise}>
+      <PaymentForm />
+    </Elements>
+  );
+}
 
 export default App;
