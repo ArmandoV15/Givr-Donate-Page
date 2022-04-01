e7deed90429e036f5192112141f700e41fc04477
Armando Valdez <armandov1015@gmail.com>
2022-03-31 17:53:39 -0700

    updated databaase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8xqMMWBJdpwW8sh1SuswDL50aTS_0GWo",
-  authDomain: "givr-86d00.firebaseapp.com",
-  projectId: "givr-86d00",
-  storageBucket: "givr-86d00.appspot.com",
-  messagingSenderId: "645843949957",
-  appId: "1:645843949957:web:90642090e59e7c00a94cec",
-  measurementId: "G-GEJ5E98DNL",
+  apiKey: "AIzaSyDeWGV0SNHRVuLzl2RiMv1wFDBUX_kY8ig",
+  authDomain: "givr-5b981.firebaseapp.com",
+  projectId: "givr-5b981",
+  storageBucket: "givr-5b981.appspot.com",
+  messagingSenderId: "457133173925",
+  appId: "1:457133173925:web:1637681d625d88a397314c",
+  measurementId: "G-J0CDGRL38V",
 };
 
 const app = initializeApp(firebaseConfig);
