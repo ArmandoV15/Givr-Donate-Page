e55f0c8ab3013feb9829f85a53715133184442d0
Armando Valdez <armandov1015@gmail.com>
2022-05-01 13:36:02 -0700

    going live

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import PaymentForm from "./components/PaymentForm";
 
 const PUBLIC_KEY =
-  "pk_test_51JgfV5KEZyhei0VRXMcr92O7d9fxYtKD2hk20EfYByHdtLV15LOSjoMPIWAI0AuT55uZuSP9E3Xdcf17fbOwxbEm004dQgAc8g";
+  "pk_live_51JgfV5KEZyhei0VR7szmBQOYR33g54LxRPXsDHsYk7TmnnfZwrZZaOwnXtciKlV0TwxaaIf6gBVzYLM2YBPNRCxM00o3QbSlAn";
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
