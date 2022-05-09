56343d1083dc2e944ff73f3b92ecbb168dbd738a
Armando Valdez <armandov1015@gmail.com>
2022-05-08 22:35:27 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import PaymentForm from "./components/PaymentForm";
 // const PUBLIC_KEY =
 //   "pk_live_51JgfV5KEZyhei0VR7szmBQOYR33g54LxRPXsDHsYk7TmnnfZwrZZaOwnXtciKlV0TwxaaIf6gBVzYLM2YBPNRCxM00o3QbSlAn";
 
-const PUBLIC_KEY2 =
+const PUBLIC_KEY =
   "pk_test_51JgF9BCDmCaQpeqp1eV2eopbWwlqZ6OnPWJQNoCV2DeZDyYp0Nu1kFQn9fz0ncMuJgNaluBozb3sxxb9C787sTZd00QzOUMzWn";
 
-const stripeTestPromise = loadStripe(PUBLIC_KEY2);
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
 function App() {
   return (
